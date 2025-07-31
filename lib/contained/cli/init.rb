e3c4fda78b91ac887226968eace95a2357cea1b9
Kevin Sylvestre <kevin@ksylvest.com>
2025-07-31 13:25:17 -0700

    Fix typo in init

--- lib/contained/cli/init.rb
+++ lib/contained/cli/init.rb
@@ -34,11 +34,11 @@ module Contained
 
         environments:
           production:
-            hosts
+            hosts:
               - 1.2.3.4
               - 5.6.7.8
           test:
-            hosts
+            hosts:
               - 4.3.2.1
               - 8.7.6.5
       YAML
